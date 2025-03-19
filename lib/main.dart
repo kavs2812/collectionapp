a5b1b211b74cb4578b09c366081d67c1cbd6aea7
kavs2812 <heysnowman57@gmail.com>
2025-03-19 16:18:35 +0530

    report  updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
 import 'dart:convert';
 import 'field_model.dart';
 import 'settings_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Hive.init('hive_data');
+  await Hive.initFlutter(
+      'hive_data'); // Use Hive.initFlutter() for Flutter apps
   Hive.registerAdapter(FieldModelAdapter());
   await Hive.openBox<String>('settings');
   await Hive.openBox<List>('collection_data');
@@ -24,7 +27,7 @@ class _MyAppState extends State<MyApp> {
   final List<Widget> _screens = [
     SettingsScreen(),
     CollectionScreen(),
-    ReportScreen(),
+    ReportsScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -180,11 +183,14 @@ class _CollectionScreenState extends State<CollectionScreen> {
       Map<String, String> newData = {};
       for (var field in fields) {
         newData[field.name] = _controllers[field.name]!.text;
-        _controllers[field.name]!.clear();
       }
+      // Add timestamp to the data
+      newData['date'] =
+          DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
 
       setState(() {
         savedData.add(newData);
+        _controllers.forEach((key, controller) => controller.clear());
       });
 
       dataBox.put('data', savedData);
@@ -273,12 +279,363 @@ class _CollectionScreenState extends State<CollectionScreen> {
   }
 }
 
-class ReportScreen extends StatelessWidget {
+class ReportsScreen extends StatefulWidget {
+  @override
+  _ReportsScreenState createState() => _ReportsScreenState();
+}
+
+class _ReportsScreenState extends State<ReportsScreen> {
+  late Box<List> collectionBox;
+  String selectedFilter = "Today";
+  String? selectedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    collectionBox = Hive.box<List>('collection_data');
+    _storeDummyData();
+  }
+
+  void _storeDummyData() {
+    DateTime yesterday = DateTime.now().subtract(Duration(days: 1));
+    // ignore: unused_local_variable
+    String formattedDate = DateFormat('yyyy-MM-dd').format(yesterday);
+    List<dynamic>? existingData = collectionBox.get('data');
+
+    if (existingData == null || existingData.isEmpty) {
+      Map<String, String> dummyEntry = {
+        "Name": "John Doe",
+        "Age": "30",
+        "Number": "1234567890",
+        "Amount": "500",
+        "Address": "123 Poultry Street",
+        "date": DateFormat('yyyy-MM-dd HH:mm:ss').format(yesterday),
+      };
+      setState(() {
+        collectionBox.put('data', [dummyEntry]);
+      });
+    }
+  }
+
+  List<Map<String, String>> getFilteredCollectionInfo({String? specificDate}) {
+    String today = DateFormat('yyyy-MM-dd').format(DateTime.now());
+    String yesterday = DateFormat('yyyy-MM-dd')
+        .format(DateTime.now().subtract(Duration(days: 1)));
+
+    List<dynamic>? storedData = collectionBox.get('data');
+    List<Map<String, String>> collectionInfo = (storedData ?? [])
+        .map((item) => Map<String, String>.from(item as Map))
+        .toList();
+
+    if (specificDate != null) {
+      return collectionInfo
+          .where((entry) =>
+              entry["date"]?.toString().startsWith(specificDate) ?? false)
+          .toList();
+    }
+
+    if (selectedFilter == "Today") {
+      return collectionInfo
+          .where(
+              (entry) => entry["date"]?.toString().startsWith(today) ?? false)
+          .toList();
+    } else if (selectedFilter == "Yesterday") {
+      return collectionInfo
+          .where((entry) =>
+              entry["date"]?.toString().startsWith(yesterday) ?? false)
+          .toList();
+    }
+
+    return collectionInfo;
+  }
+
+  void _showDateDataPopup(String selectedDate) {
+    List<Map<String, String>> filteredData =
+        getFilteredCollectionInfo(specificDate: selectedDate);
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Data for $selectedDate",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              filteredData.isNotEmpty
+                  ? SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: filteredData.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            margin: EdgeInsets.symmetric(vertical: 4.0),
+                            child: ListTile(
+                              title: Text('Entry ${index + 1}'),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: filteredData[index]
+                                    .entries
+                                    .map((e) => Text('${e.key}: ${e.value}'))
+                                    .toList(),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  : Center(child: Text("No data available for this date.")),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _pickDate() async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        selectedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+      });
+      _showDateDataPopup(selectedDate!);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Map<String, String>> collectionInfo = getFilteredCollectionInfo();
+
     return Scaffold(
-      appBar: AppBar(title: Text('Report')),
-      body: Center(child: Text('Report Screen')),
+      appBar: AppBar(
+        title: Text("Reports"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: _pickDate,
+              child: Text(selectedDate ?? "Select Date"),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _filterButton("Today"),
+                _filterButton("Yesterday"),
+                _filterButton("This Week"),
+                _filterButton("This Month"),
+              ],
+            ),
+            SizedBox(height: 16),
+            _buildSummaryGrid(collectionInfo), // Updated type
+            SizedBox(height: 8),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Recent Collection",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ),
+            SizedBox(height: 8),
+            Expanded(
+              child: collectionInfo.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: collectionInfo.length,
+                      itemBuilder: (context, index) {
+                        return _listItem(collectionInfo[index], index);
+                      },
+                    )
+                  : Center(
+                      child: Text(
+                          "No data available for ${selectedDate ?? selectedFilter}")),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSummaryGrid(List<Map<String, String>> collectionInfo) {
+    return GridView.count(
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
+      childAspectRatio: 6,
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        _summaryCard(
+            "Total Collection", _calculateTotal(collectionInfo, "count")),
+        _summaryCard("Previous Collection", _calculatePreviousTotal()),
+        _summaryCard("Total Clients", _calculateTotalClients(collectionInfo)),
+        _summaryCard("Total Payments", _calculateTotalPayments(collectionInfo)),
+      ],
+    );
+  }
+
+  String _calculateTotal(List<Map<String, String>> collectionInfo, String key) {
+    return collectionInfo.length.toString();
+  }
+
+  String _calculateTotalPayments(List<Map<String, String>> collectionInfo) {
+    double total = collectionInfo.fold(0.0, (sum, item) {
+      return sum + (double.tryParse(item["Amount"] ?? '0') ?? 0.0);
+    });
+    return total.toStringAsFixed(2);
+  }
+
+  String _calculatePreviousTotal() {
+    DateTime now = DateTime.now();
+    List<dynamic>? storedData = collectionBox.get('data');
+    List<Map<String, String>> collectionInfo = (storedData ?? [])
+        .map((item) => Map<String, String>.from(item as Map))
+        .toList();
+
+    String filterDate = "";
+
+    if (selectedFilter == "Today") {
+      DateTime yesterday = now.subtract(Duration(days: 1));
+      filterDate = DateFormat('yyyy-MM-dd').format(yesterday);
+    } else if (selectedFilter == "Yesterday") {
+      DateTime lastWeekSameDay = now.subtract(Duration(days: 7));
+      filterDate = DateFormat('yyyy-MM-dd').format(lastWeekSameDay);
+    } else if (selectedFilter == "This Week") {
+      DateTime lastMonday = now.subtract(Duration(days: now.weekday + 6));
+      DateTime lastSunday = lastMonday.add(Duration(days: 6));
+      return collectionInfo
+          .where((entry) {
+            String date = entry["date"] ?? "";
+            return date.compareTo(
+                        DateFormat('yyyy-MM-dd').format(lastMonday)) >=
+                    0 &&
+                date.compareTo(DateFormat('yyyy-MM-dd').format(lastSunday)) <=
+                    0;
+          })
+          .length
+          .toString();
+    } else if (selectedFilter == "This Month") {
+      DateTime firstDayPrevMonth = DateTime(now.year, now.month - 1, 1);
+      DateTime lastDayPrevMonth = DateTime(now.year, now.month, 0);
+      return collectionInfo
+          .where((entry) {
+            String date = entry["date"] ?? "";
+            return date.compareTo(
+                        DateFormat('yyyy-MM-dd').format(firstDayPrevMonth)) >=
+                    0 &&
+                date.compareTo(
+                        DateFormat('yyyy-MM-dd').format(lastDayPrevMonth)) <=
+                    0;
+          })
+          .length
+          .toString();
+    }
+
+    return collectionInfo
+        .where((entry) => entry["date"]?.startsWith(filterDate) ?? false)
+        .length
+        .toString();
+  }
+
+  String _calculateTotalClients(List<Map<String, String>> collectionInfo) {
+    Set<String> uniqueClients = {};
+    for (var entry in collectionInfo) {
+      String? clientName = entry["Name"]?.trim();
+      String? mobileNumber = entry["Number"]?.trim();
+      if (clientName != null &&
+          mobileNumber != null &&
+          clientName.isNotEmpty &&
+          mobileNumber.isNotEmpty) {
+        uniqueClients.add("$clientName|$mobileNumber");
+      }
+    }
+    return uniqueClients.length.toString();
+  }
+
+  Widget _summaryCard(String title, String value) {
+    return Container(
+      padding: EdgeInsets.all(6),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 2,
+            offset: Offset(1, 1),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.w600, fontSize: 11),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 2),
+          Text(
+            value,
+            style: TextStyle(
+                color: Colors.black, fontSize: 13, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _filterButton(String text) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor:
+            selectedFilter == text ? Colors.blue : Colors.grey[300],
+        foregroundColor: selectedFilter == text ? Colors.white : Colors.black,
+      ),
+      onPressed: () {
+        setState(() {
+          selectedFilter = text;
+          selectedDate = null;
+        });
+      },
+      child: Text(text),
+    );
+  }
+
+  Widget _listItem(Map<String, String> item, int index) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 4.0),
+      child: ListTile(
+        title: Text('Entry ${index + 1}'),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:
+              item.entries.map((e) => Text('${e.key}: ${e.value}')).toList(),
+        ),
+      ),
     );
   }
 }
